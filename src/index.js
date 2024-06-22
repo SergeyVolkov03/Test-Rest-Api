9803cb0d09d3711b22a17108ceae38c15ee1fb27
Sergey Volkov <serzhvolkov03@gmail.com>
2024-06-22 20:47:51 +0300

    feat: add docker compose and docker files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import "dotenv/config"
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+app.get('/', (reg, res)=>{
+    return res.json({message: "Our server is woriking"})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running  on PORT: ${PORT}`)
+})
