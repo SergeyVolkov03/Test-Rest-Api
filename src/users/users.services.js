45a6b426a44926d9301f94608bcee37260fd2b71
Sergey Volkov <serzhvolkov03@gmail.com>
2024-06-24 17:50:02 +0300

    feat: add users services

---
+++ src/users/users.services.js
@@ -0,0 +1,23 @@
+import { db } from '../../utils/db';
+
+export function createUserByEmailAndPassword(user) {
+  return db.user.create({
+    data: user,
+  });
+}
+
+export function findUserById(id) {
+  return db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+export function findUserByEmail(email) {
+    return db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
