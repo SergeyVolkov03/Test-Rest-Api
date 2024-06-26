a5ac32b688ea879b02801d727295f410f949bbca
Sergey Volkov <serzhvolkov03@gmail.com>
2024-06-26 18:08:42 +0300

    feat: implement update and delete todo by id

--- src/tasks/tasks.routes.js
+++ src/tasks/tasks.routes.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/auth.middlewares.js';
-import { createTodo, getTodos, getTodoById } from './tasks.services.js';
+import {
+  createTodo,
+  getTodos,
+  getTodoById,
+  updateTodoById,
+  deleteTodoById,
+} from './tasks.services.js';
 
 const router = express.Router();
 
@@ -30,6 +36,36 @@ router.get('/:id', isAuthenticated, async (req, res, next) => {
     const { id } = req.params;
     const { userId } = req.payload;
     const todo = await getTodoById(Number(id), userId);
+    if (!todo) {
+      res.send({ error: 'Incorrect todo id' });
+    }
+    res.json(todo);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', isAuthenticated, async (req, res, next) => {
+  try {
+    const data = req.body;
+    const { id } = req.params;
+    const { userId } = req.payload;
+    const todo = await updateTodoById(Number(id), userId, data);
+    if (!todo) {
+      res.send({ error: 'Incorrect todo id' });
+    }
+    res.json(todo);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', isAuthenticated, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.payload;
+    const todo = await deleteTodoById(Number(id), userId);
+    console.log(todo);
     if (!todo) {
       res.send({ error: 'Incorrect todo id' });
     }
--- src/tasks/tasks.services.js
+++ src/tasks/tasks.services.js
@@ -18,3 +18,11 @@ export function getTodos(userId) {
 export function getTodoById(todoId, userId) {
   return db.todo.findUnique({ where: { id: todoId, user_id: userId } });
 }
+
+export function updateTodoById(todoId, userId, data) {
+  return db.todo.update({ where: { id: todoId, user_id: userId }, data });
+}
+
+export function deleteTodoById(todoId, userId) {
+  return db.todo.delete({ where: { id: todoId, user_id: userId } });
+}
