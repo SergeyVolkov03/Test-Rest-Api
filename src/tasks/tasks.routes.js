bc276e2bea38b3e9b2cbe7c0c7e994ce1efd0894
Sergey Volkov <serzhvolkov03@gmail.com>
2024-06-26 13:04:11 +0300

    feat: implement get all todos and get todo by id

--- src/tasks/tasks.routes.js
+++ src/tasks/tasks.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/auth.middlewares.js';
-import { createTodo } from './tasks.services.js';
+import { createTodo, getTodos, getTodoById } from './tasks.services.js';
 
 const router = express.Router();
 
@@ -9,6 +9,30 @@ router.post('/', isAuthenticated, async (req, res, next) => {
     const data = req.body;
     const { userId } = req.payload;
     const todo = await createTodo(data, userId);
+    res.json(todo);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/', isAuthenticated, async (req, res, next) => {
+  try {
+    const { userId } = req.payload;
+    const todos = await getTodos(userId);
+    res.json(todos);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', isAuthenticated, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.payload;
+    const todo = await getTodoById(Number(id), userId);
+    if (!todo) {
+      res.send({ error: 'Incorrect todo id' });
+    }
     res.json(todo);
   } catch (err) {
     next(err);
--- src/tasks/tasks.services.js
+++ src/tasks/tasks.services.js
@@ -10,3 +10,11 @@ export function createTodo(todo, userId) {
     },
   });
 }
+
+export function getTodos(userId) {
+  return db.todo.findMany({ where: { user_id: userId } });
+}
+
+export function getTodoById(todoId, userId) {
+  return db.todo.findUnique({ where: { id: todoId, user_id: userId } });
+}
